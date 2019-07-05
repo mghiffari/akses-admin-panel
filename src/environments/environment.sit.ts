23fb7a1bd51a103ffa523d2730e85c39fb23d190
Natasha Janice <natasha@id.ibm.com>
2019-07-05 14:45:19 +0700

    init SIT environment

---
+++ src/environments/environment.sit.ts
@@ -0,0 +1,40 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: true,
+  appName: 'Akses',
+  apiurl: 'http://sit.aksesadirafinance.com/',
+  branchCSVFileExampleUrl: 'http://adira-akses-dev.oss-ap-southeast-5.aliyuncs.com/branch/branch_2019040811570583.csv',
+  enableAdiraEmailValidation: false,
+  version: '1.0.20',
+  versionDate: new Date(2019, 5, 20),
+  tinyMceSettings: {
+    inline: false,
+    statusbar: false,
+    browser_spellcheck: true,
+    height: 320,
+    plugins: ["lists", "table"],
+    toolbar:
+      "undo redo | formatselect | fontsizeselect | bold italic | forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | table",
+    menu: {
+      file: { title: 'File', items: 'newdocument' },
+      edit: { title: 'Edit', items: 'undo redo | cut copy paste pastetext | selectall' },
+      insert: { title: 'Insert', items: 'inserttable' },
+      view: { title: 'View', items: 'visualaid' },
+      format: { title: 'Format', items: 'bold italic underline strikethrough superscript subscript codeformat | formats blockformats fontsize align | removeformat' },
+      table: { title: 'Table', items: 'inserttable tableprops deletetable row column cell' }
+    }
+  }
+};
+
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
