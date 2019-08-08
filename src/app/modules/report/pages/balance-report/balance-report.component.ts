c4c4ce4e571d8e73e24c27c56931fc5d2f5740ea
Lusiana Wijayanti <lusiana.wijayanti@ibm.com>
2019-08-08 17:40:51 +0700

    [AA-1474] [FE] - Screen to show the Akses Pay  transaction and Balance list for the duration - Accordian -  CS group

--- src/app/modules/report/pages/balance-report/balance-report.component.ts
+++ src/app/modules/report/pages/balance-report/balance-report.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef  } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+import { BalanceData, TransBalanceData } from '../../models/balance-data';
+import { CustomValidation } from 'src/app/shared/form-validation/custom-validation';
 
 @Component({
   selector: 'app-balance-report',
@@ -6,10 +10,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./balance-report.component.scss']
 })
 export class BalanceReportComponent implements OnInit {
+  balanceColumn: string[] = [
+    'oid',
+    'vaNumber',
+    'name',
+    'phoneNumber',
+    'aksesPayBalance',
+    'timestamp'
+  ];
+  transactionColumn: string[] = [
+    'transCode',
+    'transDesc',
+    'amount',
+    'VANumberDest',
+    'sourceVANumber',
+    'transDate',
+    'transSequence'
+  ];
+  balanceData: BalanceData[] = [];
+  transData: TransBalanceData[] = [];
 
-  constructor() { }
+  search: string;
+  locale = 'id';
+  loading = false;
+  searchValidation = CustomValidation.transactionSearch;
 
+  private balanceTable: any;
+  @ViewChild('balanceTable') set balanceTabl(table: ElementRef) {
+    this.balanceTable = table;
+  }
+
+  private transTable: any;
+  @ViewChild('transTable') set transTabl(table: ElementRef) {
+    this.transTable = table;
+  }
+
+  // constructor
+  constructor(
+    private translateService: TranslateService,
+  ) {
+    console.log('BalanceReportComponent | constructor');
+  }
+
+  // component on init
   ngOnInit() {
+    console.log('BalanceReportComponent | ngOnInit')
+    this.translateService.get('angularLocale').subscribe(res => {
+      this.locale = res;
+    });
+  }
+
+  // call api to load data by search
+  onSearch() {
+    console.log('BalanceReportComponent | onSearch')
+    console.log(this.search);
+  };
+
+  // check 
+  isSearchValid() {
+    console.log('BalanceReportComponent | isSearchValid')
+    return this.search !== undefined && this.search !== null && this.search !== '' && (this.search.length > this.searchValidation.minLength);
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,7 @@ export const environment = {
   production: false,
   appName: 'Akses',
   apiurl: 'http://149.129.250.31:30001/api/v1/',
+  msPaymentApiUrl: 'http://149.129.250.31:30011/api/v1/',
   branchCSVFileExampleUrl: 'http://adira-akses-dev.oss-ap-southeast-5.aliyuncs.com/branch/branch_2019040811570583.csv',
   endPoint: {
     updateAccount: 'account',
