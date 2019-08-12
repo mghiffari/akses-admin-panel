61db2bee06bd7a8b7968c8a825dfd44b8445345b
Natasha Janice <natasha@id.ibm.com>
2019-08-12 10:31:04 +0700

    fix conflict

--- src/app/shared/services/product.service.spec.ts
+++ src/app/shared/services/product.service.spec.ts
@@ -2,11 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { ProductService } from './product.service';
 
-<<<<<<< HEAD
 describe('ProductService', () => {
-=======
-describe('EncryptionService', () => {
->>>>>>> sprint_10
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
