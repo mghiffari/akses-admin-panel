90d4d37a99c03c92d856f1b19b6320dff5ac0658
Natasha Janice <natasha@id.ibm.com>
2019-10-18 11:29:14 +0700

    delete length validation in report search

--- src/app/modules/report/pages/balance-report/balance-report.component.ts
+++ src/app/modules/report/pages/balance-report/balance-report.component.ts
@@ -45,7 +45,6 @@ export class BalanceReportComponent implements OnInit {
   search: string;
   locale = 'id';
   loading = false;
-  searchValidation = CustomValidation.transactionSearch;
   isFocusedInput = false;
 
   private balanceTable: any;
@@ -144,7 +143,13 @@ export class BalanceReportComponent implements OnInit {
     if(event){
       this.search === event.target.value
     } 
-    return this.search !== undefined && this.search !== null && this.search !== '' && (this.search.length >= this.searchValidation.minLength);
+    if(this.search !== undefined && this.search !== null && this.search !== ''){
+      return true;
+    } else {
+      this.balanceData = [];
+      this.transData = [];
+      return false;
+    }
   }
 
 }
