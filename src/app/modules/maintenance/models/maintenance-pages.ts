23063727956971d25cd7d1e1ed8d815ecd7e9a67
iqbal <iqbal@id.ibm.com>
2019-12-13 17:32:01 +0700

    add definition map pages maintenance mode AD-164

---
+++ src/app/modules/maintenance/models/maintenance-pages.ts
@@ -0,0 +1,10 @@
+export class MaintenancePages {
+  contract_list_page = "Contract List";
+  history_payment_page = "History Payment";
+  submit_page = "Submit Payment";
+  special_offermpl_page = "Special Offer MPL";
+  withdrawal_page = "Withdrawal";
+  special_offer_page = "Special Offer";
+  contract_details_page = "Contract Details";
+  history_wallet_page = "History Wallet";
+};
