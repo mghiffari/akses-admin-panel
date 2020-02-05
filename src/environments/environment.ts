cd566eac29b403bd4a2fd8fcc092be1cf3e63d89
Bobby <BobbyIgnatius@ymail.com>
2020-02-05 15:21:37 +0700

    change environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // apiurl: 'https://dev.aksesadirafinance.com/ms-admin/',
-  apiurl: 'http://localhost:8080/api/v1/',
+  apiurl: 'https://dev.aksesadirafinance.com/ms-admin/',
+  // apiurl: 'http://localhost:8080/api/v1/',
   endPoint: {
     updateAccount: 'account',
     login: 'auth/login',
